ba0ccbdca37af3f39d2a6a8e5e25aaf5c41b16cb
ahmedsadman <ahmedsadman.211@gmail.com>
2021-01-29 18:18:16 +0600

    sync CRUD events with producer-consumer

--- admin/products/producer.py
+++ admin/products/producer.py
@@ -1,4 +1,5 @@
 import os
+import json
 import pika
 
 params = pika.URLParameters(os.environ.get('RABBITMQ_SERVER_URI'))
@@ -6,5 +7,7 @@ connection = pika.BlockingConnection(params)
 channel = connection.channel()
 
 
-def publish():
-    channel.basic_publish(exchange='', routing_key='main', body='hello')
+def publish(method, body):
+    properties = pika.BasicProperties(method)
+    channel.basic_publish(exchange='', routing_key='main',
+                          body=json.dumps(body), properties=properties)
--- main/consumer.py
+++ main/consumer.py
@@ -1,5 +1,7 @@
 import os
+import json
 import pika
+from main import Product, db
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -13,10 +15,32 @@ channel.queue_declare(queue='main')
 
 def callback(ch, method, properties, body):
     print('Received in main')
-    print(body)
+    data = json.loads(body)
+    print(data)
+
+    if properties.content_type == 'product_created':
+        product = Product(
+            id=data['id'], title=data['title'], image=data['image'])
+        db.session.add(product)
+        db.session.commit()
+        print('Product Created')
+
+    elif properties.content_type == 'product_updated':
+        product = Product.query.get(data['id'])
+        product.title = data['title']
+        product.image = data['image']
+        db.session.commit()
+        print('Product Updated')
+
+    elif properties.content_type == 'product_deleted':
+        product = Product.query.get(data)
+        db.session.delete(product)
+        db.session.commit()
+        print('Product Deleted')
 
 
-channel.basic_consume(queue='main', on_message_callback=callback, auto_ack=True)
+channel.basic_consume(
+    queue='main', on_message_callback=callback, auto_ack=True)
 print('Started Consuming')
 channel.start_consuming()
 channel.close()
